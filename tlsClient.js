41891bc8fc3ebed80d719fca839a958218251ca7
DoresaO <doresa.osmanaj@student.uni-pr.edu>
2022-05-15 11:03:41 +0200

    adding to client side

--- tlsClient.js
+++ tlsClient.js
@@ -29,3 +29,34 @@ let encryptedData = cipher.update(message, "utf-8", "hex");
 encryptedData += cipher.final("hex");
 
 //console.log("Encrypted message: " + encryptedData);
+
+
+
+// Kalojini certifikatat serverit dhe njoftojeni që të përpunojë edhe certifikata të paautorizuara. 
+var options = { 
+    key:fs.readFileSync('private-key.pem'), 
+    cert:fs.readFileSync('public-cert.pem'), 
+    rejectUnauthorized:false 
+    }; 
+var client = tls.connect(PORT, HOST, options, function() { 
+ // Kontrolloni nëse autorizimi funksionoi
+if (client.authorized) { 
+    console.log("Connection authorized by a Certificate Authority."); 
+        } else { 
+    console.log(`Connection not authorized: ${client.authorizationError}`); 
+        } 
+ // Dërgo një mesazh miqësor
+client.write(encryptedData.toString()); 
+    }); 
+client.on("data", (data) => { 
+    console.log(`Received: ${data}`); 
+    }); 
+client.on('close', () => { 
+    console.log("Connection closed"); 
+    });  
+ // Kur një gabim shfaqet, tregoje atë.
+client.on('error', (error) => { 
+    console.error(error); 
+// Mbyllni lidhjen pasi gabimi ka ndodhur gabimi.
+    client.destroy(); 
+    }); 
